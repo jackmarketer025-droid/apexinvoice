f6107a913103565ba273e2aea95fa5d7ca044d39
jackpyt11 <jononicomputer21@gmail.com>
2026-04-05 23:07:34 +0600

    chore: remove redundant apexinvoice directory

--- apexinvoice/src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-5039185866-ac16d",
-  "appId": "1:700611381306:web:d349509040984709cbcd03",
-  "apiKey": "AIzaSyCdmvDnXLdoMAUF0cDGIdz1IzkWrolRNCU",
-  "authDomain": "studio-5039185866-ac16d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "700611381306"
-};
